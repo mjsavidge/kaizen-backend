60d712943d7da73cc7db0144ac364b26e36537c2
mjsavidge <67027226+mjsavidge@users.noreply.github.com>
2022-02-14 21:51:36 -0500

    4th commit fixed some bugs is repo and service forgetting to at the @repository and extend JPA, for service forgetting to add the @service

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 
 import model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     Optional<UserModel> findByEmail(String email);
 }
--- src/main/java/service/RegistrationService.java
+++ src/main/java/service/RegistrationService.java
@@ -1,7 +1,9 @@
 package service;
 
 import model.RegistrationModel;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationModel request){
         return "works";
